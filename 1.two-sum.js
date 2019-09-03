4e96ab3636d7f542a34f4d5a9f768a5367b6b1c3
jiaoguanwen <jiaoguanwen@163.com>
2019-09-03 16:46:28 +0800

    add map

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -18,3 +18,13 @@ var twoSum = function(nums, target) {
   }
   throw Error('No two sum solution');
 };
+/* var twoSum = function(nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i];
+    }
+    comp[target - nums[i]] = i;
+  }
+  throw Error('No two sum solution');
+}; */
